17bc865108f0d93db2cb11db86d6c7235a6409d4
JoshLarouche <joshua.t.larouche@gmail.com>
2023-09-30 16:28:49 -0700

    changing approach and added styling and search

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -1,10 +1,85 @@
-import React from 'react';
-import Webpages from './webpages';
-function App() {
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import axios from "axios";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [nameQuery, setNameQuery] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let res = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+        let allData = res.data.filter((data) => {
+          if (nameQuery) {
+            return data.name.toLowerCase().includes(nameQuery.toLowerCase());
+          }
+          else {
+            return data;
+          }
+        });
+        setData(allData);
+        console.log(allData);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [nameQuery]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
-      <Webpages />
+      <h1>Search For Contact Info</h1>
+
+      <input
+        name="search"
+        className="search__input"
+        onChange={(e) => setNameQuery(e.target.value)}
+        value={nameQuery}
+        type="text"
+        placeholder="search by name"
+      />
+      <div>
+        {data.map((user) => (
+          <List user={user} key={user.id} />
+        ))}
+      </div>
     </div>
   );
 }
-export default App;
+
+const List = ({ post, setData, user }) => {
+  const { name, email, phone, website, company } = user;
+
+  return (
+    <>
+      <div className="contact__container">
+        <div className="contact__item">
+          <span className="bold">Name:</span> {name}
+        </div>
+        <div className="contact__item">
+          <span className="bold">Email: </span>
+          {email}
+        </div>
+        <div className="contact__item">
+          <span className="bold">Phone: </span>
+          {phone}
+        </div>
+        <div className="contact__item">
+          <span className="bold">Website:</span>
+          {website}
+        </div>
+        <div className="contact__item">
+          <span className="bold">Company Name:</span>
+          {company.name}
+        </div>
+      </div>
+    </>
+  );
+};
