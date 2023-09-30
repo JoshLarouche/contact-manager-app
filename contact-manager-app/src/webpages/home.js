1becb34b2540a33805ebb6dbecc25c3c2eb233b3
JoshLarouche <joshua.t.larouche@gmail.com>
2023-09-30 16:00:33 -0700

    debugging

--- contact-manager-app/src/webpages/home.js
+++ contact-manager-app/src/webpages/home.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
 const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users/")
-            .then(res => res.json())
-            .then(
-                (data) => {
-                    setIsLoaded(true);
-                    setUsers(data);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-      }, [])
+const [isLoaded, setIsLoaded] = useState(false);
+const [users, setUsers] = useState([]);
+useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+        .then(res => res.json())
+        .then(
+            (data) => {
+                setIsLoaded(true);
+                setUsers(data);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+        )
+    }, [])
 if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
