418a8487055e2cc33d9d2f6aba292b4e3a6abf36
JoshLarouche <joshua.t.larouche@gmail.com>
2023-09-30 15:18:29 -0700

    switch to react app

---
+++ contact-manager-app/src/webpages/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './home';
+import User from './user';
+const Webpages = () => {
+    return(
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} exact />
+            <Route path = "/user/:id" element={<User/>} />
+          </Routes>
+        </BrowserRouter>
+    );
+};
+export default Webpages;
